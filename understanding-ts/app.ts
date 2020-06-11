52429c45bb55902e30934467d9d807cc13483027
IAMBakker <icobakker@valori.nl>
2020-06-11 14:21:37 +0200

    tuples and arrays in typescript

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -2,9 +2,26 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: "Ico",
-    age: 30
+    age: 32,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "Tester"]
 };
 
+person.role.push("Admin");
+// person.role[1] = 10;
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby);
+}
